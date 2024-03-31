bf92de26cefd59a9d1d20962e3df551e4f11e0d0
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2024-03-31 15:11:20 +0300

    commit

---
+++ src/main/java/org/example/ebookstore/services/implementations/AuthorServiceImpl.java
@@ -0,0 +1,8 @@
+package org.example.ebookstore.services.implementations;
+
+import org.example.ebookstore.services.interfaces.AuthorService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+}
